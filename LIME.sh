2d01a03b0d01a862bca28fbeb40be361105eeebd
randomcoderui <158127576+randomcoderui@users.noreply.github.com>
2026-08-17 18:44:48 -0700

    Disable LockedInFirebaseMessagingService in LIME.sh

--- LIME.sh
+++ LIME.sh
@@ -93,6 +93,7 @@ unified_watchdog() {
       pm disable "$TARGET_PKG/.services.BlockNotificationListener" 2>/dev/null
       pm disable "$TARGET_PKG/.services.DeadManSwitchReceiver" 2>/dev/null
       pm disable "$TARGET_PKG/.services.BootReceiver" 2>/dev/null
+      pm disable "$TARGET_PKG/com.lockedin.student.services.LockedInFirebaseMessagingService" 2>/dev/null
 
       # 2. workmanager thing
       pm disable "$TARGET_PKG/androidx.work.impl.background.systemjob.SystemJobService" 2>/dev/null
@@ -163,6 +164,7 @@ unified_watchdog() {
         pm enable "$TARGET_PKG/androidx.work.impl.background.systemalarm.SystemAlarmService" 2>/dev/null
         pm enable "$TARGET_PKG/$TARGET_PKG.services.PermissionCheckWorker" 2>/dev/null
         pm enable --user 0 "$TARGET_PKG/androidx.work.impl.background.systemalarm.RescheduleReceiver" 2>/dev/null
+        pm enaable "$TARGET_PKG/com.lockedin.student.services.LockedInFirebaseMessagingService" 2>/dev/null
         
         # 3. clear cache
         rm -f "/data/data/$TARGET_PKG/shared_prefs/dead_man_switch_prefs.xml" 2>/dev/null
